c9831d05009dbf15c4d92940238e049804c59535
lakshit1307 <lakshit1307@gmail.com>
2017-12-12 16:49:17 +0530

    table name change

--- src/main/java/com/healthedge/codeloaders/repository/ClientServiceRepositoryImpl.java
+++ src/main/java/com/healthedge/codeloaders/repository/ClientServiceRepositoryImpl.java
@@ -17,7 +17,7 @@ public class ClientServiceRepositoryImpl implements ClientServiceRepository {
 	@Override
 	public Date getPayorCodeVersion(String codeType, EntityManager entityManager) {
 
-		Query query = entityManager.createNativeQuery("SELECT EFF_START_DT FROM (SELECT * FROM SERVICES "
+		Query query = entityManager.createNativeQuery("SELECT EFF_START_DT FROM (SELECT * FROM SERVICE "
 				+ "WHERE SERV_TYPE_CD= ?1 ORDER BY EFF_START_DT DESC) WHERE ROWNUM=1");
 		query.setParameter(1, codeType);
 		List<Date> dates = query.getResultList();
@@ -31,7 +31,7 @@ public class ClientServiceRepositoryImpl implements ClientServiceRepository {
 	public List<String> getDistinctPayorCodes(EntityManager entityManager) {
 
 		List<String> distinctCodes;
-		distinctCodes = entityManager.createNativeQuery("SELECT DISTINCT SERV_TYPE_CD FROM SERVICES").getResultList();
+		distinctCodes = entityManager.createNativeQuery("SELECT DISTINCT SERV_TYPE_CD FROM SERVICE").getResultList();
 		return distinctCodes;
 	}
 
@@ -40,7 +40,7 @@ public class ClientServiceRepositoryImpl implements ClientServiceRepository {
 		entityManager.getTransaction().begin();
 		for (ClientService clientService : clientServices) {
 			Query query = entityManager.createNativeQuery(
-					"UPDATE SERVICES SET SERV_SHORT_DSC= ?1,SERV_LONG_DSC= ?2,ALT_DSC= ?3,EFF_START_DT=?4,EFF_END_DT=?5 WHERE SERV_CD= ?6");
+					"UPDATE SERVICE SET SERV_SHORT_DSC= ?1,SERV_LONG_DSC= ?2,ALT_DSC= ?3,EFF_START_DT=?4,EFF_END_DT=?5 WHERE SERV_CD= ?6");
 			query.setParameter(1, clientService.getServiceShortDesciption());
 			query.setParameter(2, clientService.getServiceLongDesciption());
 			query.setParameter(3, clientService.getServiceAlternateDesciption());
@@ -56,7 +56,7 @@ public class ClientServiceRepositoryImpl implements ClientServiceRepository {
 	public void terminate(EntityManager entityManager, List<ClientService> clientServices) {
 		entityManager.getTransaction().begin();
 		for (ClientService clientService : clientServices) {
-			Query query = entityManager.createNativeQuery("UPDATE SERVICES SET EFF_END_DT= ?1 WHERE SERV_CD= ?2");
+			Query query = entityManager.createNativeQuery("UPDATE SERVICE SET EFF_END_DT= ?1 WHERE SERV_CD= ?2");
 			query.setParameter(1, clientService.getEffectiveEndDate());
 			query.setParameter(2, clientService.getServiceCode());
 			query.executeUpdate();
@@ -78,7 +78,7 @@ public class ClientServiceRepositoryImpl implements ClientServiceRepository {
 	@Override
 	public List<String> getDistinctPayorCodes(EntityManager entityManager, String codeType) {
 
-		Query query = entityManager.createNativeQuery("SELECT SERV_CD FROM SERVICES WHERE SERV_TYPE_CD= ?1");
+		Query query = entityManager.createNativeQuery("SELECT SERV_CD FROM SERVICE WHERE SERV_TYPE_CD= ?1");
 		query.setParameter(1, codeType);
 		List<String> payorCodes = query.getResultList();
 		return payorCodes;
