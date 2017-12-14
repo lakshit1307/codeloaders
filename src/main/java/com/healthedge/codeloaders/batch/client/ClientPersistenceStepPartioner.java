c8a65afdff28144b83419d0c773a542a856ca44a
lakshit1307 <lakshit1307@gmail.com>
2017-12-14 14:58:25 +0530

    Creation of partition only for tenants and tenant environment available for auto load

--- src/main/java/com/healthedge/codeloaders/batch/client/ClientPersistenceStepPartioner.java
+++ src/main/java/com/healthedge/codeloaders/batch/client/ClientPersistenceStepPartioner.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import com.healthedge.codeloaders.entity.Tenant;
 import com.healthedge.codeloaders.entity.TenantEnv;
 
 public class ClientPersistenceStepPartioner implements Partitioner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ClientPersistenceStepPartioner.class);
 
 	@Autowired
 	private TenantDao tenantDao;
@@ -33,22 +37,45 @@ public class ClientPersistenceStepPartioner implements Partitioner {
 		Map<String, ExecutionContext> partitionMap = new HashMap<String, ExecutionContext>();
 		if (tenantId.equals("all")) {
 			for (Tenant tenant : tenantDao.getAllTenant()) {
-				for (TenantEnv tenantEnv : tenant.getTenantEnv()) {
-					ExecutionContext context = new ExecutionContext();
-					context.put("tenant_environment", tenantEnv);
-					partitionMap.put(tenant.getTenantId() + "_" + tenantEnv.getTenantEnvId(), context);
+				if (validateTenant(tenant)) {
+					for (TenantEnv tenantEnv : tenant.getTenantEnv()) {
+						if (validateTenantEnv(tenantEnv)) {
+							ExecutionContext context = new ExecutionContext();
+							context.put("tenant_environment", tenantEnv);
+							partitionMap.put(tenant.getTenantId() + "_" + tenantEnv.getTenantEnvId(), context);
+						}
 
+					}
 				}
 			}
 		} else {
 			TenantEnv tenantEnv = tenantEnvDao.geTenantEnvById(tenantEnvId);
-			ExecutionContext context = new ExecutionContext();
-			context.put("tenant_environment", tenantEnv);
-			partitionMap.put(tenantEnv.getTenant().getTenantId() + "_" + tenantEnv.getTenantEnvId(), context);
+			if (validateTenant(tenantEnv.getTenant()) && validateTenantEnv(tenantEnv)) {
+				ExecutionContext context = new ExecutionContext();
+				context.put("tenant_environment", tenantEnv);
 
+			}
 		}
 
 		return partitionMap;
+	}
+
+	private boolean validateTenant(Tenant tenant) {
+		if (tenant.getIsActive() == 1 && tenant.getIsAutoLoad() == 1) {
+			return true;
+		}
+		LOGGER.info("Tenant with tenantId: " + tenant.getTenantId() + " and tenant name: " + tenant.getName()
+				+ " is not configured for auto load");
+		return false;
+	}
+
+	private boolean validateTenantEnv(TenantEnv tenantEnv) {
+		if (tenantEnv.getIsActive() == 1 && tenantEnv.getIsAutoLoad() == 1) {
+			return true;
+		}
+		LOGGER.info("Tenant environment with tenantEnvId: " + tenantEnv.getTenantEnvId() + " and tenant name: "
+				+ tenantEnv.getName() + " is not configured for auto load");
+		return false;
 	}
 
 }
