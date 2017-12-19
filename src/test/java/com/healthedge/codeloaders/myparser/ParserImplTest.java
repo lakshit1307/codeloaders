7d9ee5dae4fb9bacda123991d5e1afe63e514b1a
akhil-Kumar-mg <31155395+akhil-Kumar-mg@users.noreply.github.com>
2017-12-19 14:40:13 +0530

    Procedure_Transformer Added

--- src/test/java/com/healthedge/codeloaders/myparser/ParserImplTest.java
+++ src/test/java/com/healthedge/codeloaders/myparser/ParserImplTest.java
@@ -29,15 +29,22 @@ public class ParserImplTest {
     @Test
     public void parse() throws Exception {
         File file = new File("src/test/resources/basedata/CPT/OPTUM_CPT_2016-01-01.txt");
-
         MyFileMetaData fileMetaData = new MyFileMetaData("CPT", file.getAbsolutePath());
         List<Map<String, String>> result = abstractParser.parse(fileMetaData);
-
         System.out.println(result);
         Transformer transformer = implementationFactory.getTransformer(fileMetaData.getFileType());
         Map<String, BaseEntity> tranformerResult = transformer.transform(result);
+        System.out.println(tranformerResult);
+
+        file = new File("src/test/resources/basedata/PROCEDURE/OPTUM_ICD10PCS_BASE_2018-01-01.tab");
+        fileMetaData = new MyFileMetaData("PROCEDURE", file.getAbsolutePath());
+        result = abstractParser.parse(fileMetaData);
+        System.out.println(result);
+        transformer = implementationFactory.getTransformer(fileMetaData.getFileType());
+        tranformerResult = transformer.transform(result);
 
         System.out.println(tranformerResult);
+
 
         file = new File("src/test/resources/basedata/ZipToCarrierLocality/OPTUM_ZipToCarrierLocality_2003-01-01.txt");
 
