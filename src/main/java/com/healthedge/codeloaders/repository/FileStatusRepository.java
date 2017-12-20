18d79c0952bde1ee44e36b742332e95b3bd97dd0
Arijit Das <adas@healthedge.com>
2017-12-20 17:28:40 +0530

    integration

--- src/main/java/com/healthedge/codeloaders/repository/FileStatusRepository.java
+++ src/main/java/com/healthedge/codeloaders/repository/FileStatusRepository.java
@@ -16,6 +16,10 @@ public interface FileStatusRepository extends JpaRepository<FileStatus, String> 
     @Query(value = "UPDATE CODE_FILE_STATUS SET STATUS=?1 WHERE VERSION=?2 AND FILE_TYPE=?3 AND STATUS=?4",nativeQuery = true)
 	void updateStatusByFileNameAndCodeType(String status, Long fileVersion, String fileType, String currStatus);
 
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE CODE_FILE_STATUS SET STATUS=?1 WHERE TX_CNT=?2",nativeQuery = true)
+    void updateStatusByTnxCnt(String status, Long tnxCnt);
+
     @Query(value ="SELECT * FROM CODE_FILE_STATUS WHERE FILE_TYPE=?1 AND TX_DATE=(SELECT MAX(TX_DATE) FROM CODE_FILE_STATUS WHERE FILE_TYPE=?1 AND STATUS=?2)" ,nativeQuery = true)
      FileStatus getFileTypeDetailsForLatestVersion(String fileType, String status);
 }
