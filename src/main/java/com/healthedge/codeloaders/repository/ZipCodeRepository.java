8da2ad246bdda1b9401cb624ba9e60c8db3c6a71
joseph <josefgeorge@rocketmail.com>
2017-12-20 16:49:29 +0530

    zipCodeRepository partially done

--- src/main/java/com/healthedge/codeloaders/repository/ZipCodeRepository.java
+++ src/main/java/com/healthedge/codeloaders/repository/ZipCodeRepository.java
@@ -1,7 +1,14 @@
 package com.healthedge.codeloaders.repository;
 
+
 import com.healthedge.codeloaders.entity.ZipCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ZipCodeRepository extends JpaRepository<ZipCode,Long> {
+
+    @Query(value = "SELECT * FROM ZIP WHERE VERSION_END=?1", nativeQuery = true)
+    List<ZipCode> getZipCodesForVersion(Long versionEnd);
 }
