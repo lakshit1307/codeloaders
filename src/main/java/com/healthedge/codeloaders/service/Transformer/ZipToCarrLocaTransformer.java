3e6dc107b5ff0de6d6e3bf4875a3b8e2ed105179
joseph <josefgeorge@rocketmail.com>
2017-12-22 12:43:34 +0530

    client side entity level changes for ziptocarrierlocality

--- src/main/java/com/healthedge/codeloaders/service/Transformer/ZipToCarrLocaTransformer.java
+++ src/main/java/com/healthedge/codeloaders/service/Transformer/ZipToCarrLocaTransformer.java
@@ -1,15 +1,15 @@
 package com.healthedge.codeloaders.service.Transformer;
 
 import com.healthedge.codeloaders.common.CodeLoaderConstants;
+import com.healthedge.codeloaders.dao.ZipToCarrierLocalityDao;
 import com.healthedge.codeloaders.entity.BaseEntity;
 import com.healthedge.codeloaders.entity.ClientBaseEntity;
+import com.healthedge.codeloaders.entity.ClientZipToCarrLocEntity;
 import com.healthedge.codeloaders.entity.ZipToCarrierLocality;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ZipToCarrLocaTransformer implements Transformer{
@@ -35,7 +35,35 @@ public class ZipToCarrLocaTransformer implements Transformer{
 
     @Override
     public List<ClientBaseEntity> clientEntityTransform(List<BaseEntity> input){
-        return null;
+
+        List<ClientBaseEntity> output=new ArrayList<>();
+        List<ZipToCarrierLocality> zipToCarrierLocalityList=(List<ZipToCarrierLocality>) (List<?>) input;
+
+        DateTime current = new DateTime();
+        zipToCarrierLocalityList.forEach(item->{
+            ClientZipToCarrLocEntity clientZipToCarrLocEntity=new ClientZipToCarrLocEntity();
+           // clientZipToCarrLocEntity.setZipToCarrLocalityId(); // TODO this need not be set
+            clientZipToCarrLocEntity.setEffectiveStartDate(item.getEffectiveStartDate());
+            clientZipToCarrLocEntity.setCarrierNbr(item.getCarrierNbr());
+            clientZipToCarrLocEntity.setLocalityNbr(item.getLocalityNbr());
+            clientZipToCarrLocEntity.setVersionId((long) 1); //TODO
+            clientZipToCarrLocEntity.setConceptFulfilledCd(item.getConceptFulfilledCd());
+            clientZipToCarrLocEntity.setCvcTypeCd(item.getCvcTypeCd());
+            clientZipToCarrLocEntity.setInstActiveCd(item.getInstActiveCd());
+            clientZipToCarrLocEntity.setEndorEffectiveDate(current.toDate().getTime());  //TODO
+            clientZipToCarrLocEntity.setEndorExpiryDate(current.toDate().getTime());          //TODO
+            clientZipToCarrLocEntity.setVersionEffectiveDate(current.toDate().getTime());     //TODO
+            clientZipToCarrLocEntity.setVersionExpiryDate(current.toDate().getTime());        //TODO
+            clientZipToCarrLocEntity.setScheduleId((long) 1); // TODO to read scheduled Id Medicare
+            clientZipToCarrLocEntity.setCode(item.getCode());
+            clientZipToCarrLocEntity.setZipCode(item.getZipCode());
+            clientZipToCarrLocEntity.setLastTransactionDate(current.toDate().getTime());
+            clientZipToCarrLocEntity.setTxCnt(current.toDate().getTime());
+            clientZipToCarrLocEntity.setLastTransactionUserText(CodeLoaderConstants.TRANSACTION_USER);
+            output.add(clientZipToCarrLocEntity);
+        });
+
+        return output;
     }
 
 	@Override
